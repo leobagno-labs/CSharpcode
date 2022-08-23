74ef2e726518b79af9505124b3b19effacfee61c
Leonardo Bagno <bagnoleonardo@gmail.com>
2022-08-23 11:21:18 -0300

    Add changes to my challenges

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MealCalculation
+{
+    internal class Program
+    {
+              
+        public static void solve(double mealCost, int tipPercent, int taxPercent)
+        {
+        
+
+            double tip = mealCost * tipPercent / 100;
+            double tax = mealCost * taxPercent / 100;
+            double totalCost = mealCost + tip + tax;
+
+            Console.WriteLine("The total meal cost is "  + Math.Round(totalCost) + " dollars.");
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            solve(12.00, 20, 8);
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# CSharpcode
-Operators
